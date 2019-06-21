13d6904fcdb78c2b46cf6060f40dc44627990e5d
Nick Lee <leexnick@gmail.com>
2019-06-21 12:54:37 -0700

    add cors whitelist

--- src/server.js
+++ src/server.js
@@ -3,13 +3,14 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const corsOptions = require('./cors-whitelist');
 const morgan = require('morgan');
 const app = express();
 const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common';
 const { PORT } = require('./config');
 const dogsRouter = require('./dogs/dogsRouter');
 const catsRouter = require('./cats/catsRouter');
-app.use(cors());
+app.use(cors({ origin: corsOptions }));
 
 app.use(morgan(morganSetting));
 
