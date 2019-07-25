0ee3be15b56e268d25c1fa50e063b7cfcda1db22
Nick Lee <leexnick@gmail.com>
2019-07-25 15:45:53 -0700

    fix cors-whitelist

--- src/cors-whitelist.js
+++ src/cors-whitelist.js
@@ -1,4 +1,4 @@
-const whitelist = ['http://localhost:3000', 'https://fifo-paa.now.sh/']
+const whitelist = ['http://localhost:3000', 'https://fifo-paa.now.sh']
 
 const originGenerator = function(origin, callback) {
   if (whitelist.indexOf(origin) !== -1 || !origin) {
