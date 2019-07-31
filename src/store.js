76a07eb2e8cd4872d1f27d3ae5758b5c9d25dd3e
Peter Pae <paendabear@gmail.com>
2019-07-31 19:22:25 -0400

    change dog and cat files for unique photos and descriptions

--- src/store.js
+++ src/store.js
@@ -17,9 +17,9 @@ const STORE = {
     {
       id: 2,
       imageURL:
-        'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+        'https://images.pexels.com/photos/356378/pexels-photo-356378.jpeg?auto=compress&cs=tinysrgb&h=650&w=940',
       imageDescription:
-        'A smiling golden-brown golden retreiver listening to music.',
+        'A smiling white fluffy dog enjoying the grass.',
       user_name: 'Nick',
       name: 'Hera',
       sex: 'Female',
@@ -30,9 +30,9 @@ const STORE = {
     {
       id: 3,
       imageURL:
-        'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+        'https://images.pexels.com/photos/406014/pexels-photo-406014.jpeg?auto=compress&cs=tinysrgb&h=650&w=940',
       imageDescription:
-        'A smiling golden-brown golden retreiver listening to music.',
+        'a close up of this energetic pup.',
       user_name: 'Rahim',
       name: 'Hercules',
       sex: 'Male',
@@ -43,9 +43,9 @@ const STORE = {
     {
       id: 4,
       imageURL:
-        'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+        'https://images.pexels.com/photos/1254140/pexels-photo-1254140.jpeg?auto=compress&cs=tinysrgb&h=650&w=940',
       imageDescription:
-        'A smiling golden-brown golden retreiver listening to music.',
+        'A smiling golden-brown golden retreiver taking a stroll in the forest.',
       user_name: 'Peter',
       name: 'Chord',
       sex: 'Male',
@@ -56,9 +56,9 @@ const STORE = {
     {
       id: 5,
       imageURL:
-        'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+        'https://images.pexels.com/photos/58997/pexels-photo-58997.jpeg?auto=compress&cs=tinysrgb&h=650&w=940',
       imageDescription:
-        'A smiling golden-brown golden retreiver listening to music.',
+        'A very nice corgi.',
       user_name: 'John',
       name: 'Max',
       sex: 'Male',
@@ -85,9 +85,9 @@ const STORE = {
     {
       id: 2,
       imageURL:
-        'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg',
+        'https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260',
       imageDescription:
-        'Orange bengal cat with black stripes lounging on concrete.',
+        'white and gray cat with amber eyes.',
       user_name: 'Nick',
       name: 'Scar',
       sex: 'Female',
@@ -98,9 +98,9 @@ const STORE = {
     {
       id: 3,
       imageURL:
-        'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg',
+        'https://images.pexels.com/photos/730896/pexels-photo-730896.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
       imageDescription:
-        'Orange bengal cat with black stripes lounging on concrete.',
+        'Gray fluffy cat with brown eyes.',
       user_name: 'Rahim',
       name: 'Boss',
       sex: 'Female',
@@ -111,9 +111,9 @@ const STORE = {
     {
       id: 4,
       imageURL:
-        'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg',
+        'https://images.pexels.com/photos/416160/pexels-photo-416160.jpeg?auto=compress&cs=tinysrgb&h=650&w=940',
       imageDescription:
-        'Orange bengal cat with black stripes lounging on concrete.',
+        'white Cat with brown spots',
       user_name: 'Peter',
       name: 'Ben',
       sex: 'Male',
@@ -124,9 +124,9 @@ const STORE = {
     {
       id: 5,
       imageURL:
-        'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg',
+        'https://images.pexels.com/photos/617278/pexels-photo-617278.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
       imageDescription:
-        'Orange bengal cat with black stripes lounging on concrete.',
+        'green eye striped cat',
       user_name: 'John',
       name: 'Fluffy',
       sex: 'Female',
